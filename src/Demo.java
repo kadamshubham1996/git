e86d0789b7812db2ab10ac1f3d714dd78f4799b7
rakesh16495 <rakeshmusale111@gmail.com>
2017-07-14 21:26:48 +0530

    Inital commit

--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,6 @@
-package PACKAGE_NAME;
 
-/**
- * Created by rckstr on 7/13/2017.
- */
 public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Helo");
+    }
 }
